d1e4a91079a169b335f47d69bc01519b39ad629e
Bartłomiej Soter <10560885+jabarij@users.noreply.github.com>
2021-05-28 20:15:22 +0200

    Issue #11 - Developed monads transition to/from FSharp; - Developed data ops monads transition to/from FSharp;

---
+++ src/Monads.DataOps.FSharp/ClearableValue.fs
@@ -0,0 +1,25 @@
+﻿namespace Monads.DataOps.FSharp
+
+type ClearableValue<'value> =
+    | Set of 'value
+    | Clear
+    | NoAction
+
+module ClearableValue =
+    let toFSharp<'value> (clearable: Monads.DataOps.ClearableValue<'value>): ClearableValue<'value> =
+        clearable.Match(
+            System.Func<'value, ClearableValue<'value>>(Set),
+            System.Func<ClearableValue<'value>>(fun _ -> Clear),
+            System.Func<ClearableValue<'value>>(fun _ -> NoAction))
+
+    let toCSharp<'value> (clearable: ClearableValue<'value>): Monads.DataOps.ClearableValue<'value> =
+        match clearable with
+        | Set value -> Monads.DataOps.ClearableValue<'value>.Set(value)
+        | Clear -> Monads.DataOps.ClearableValue<'value>.Clear()
+        | NoAction -> Monads.DataOps.ClearableValue<'value>.NoAction()
+
+    let map (mapper: 'a -> 'b) (clearable: ClearableValue<'a>): ClearableValue<'b> =
+        match clearable with
+        | Set value -> Set (mapper value)
+        | Clear -> Clear
+        | NoAction -> NoAction
---
+++ src/Monads.DataOps.FSharp/EditableValue.fs
@@ -0,0 +1,21 @@
+﻿namespace Monads.DataOps.FSharp
+
+type EditableValue<'value> =
+    | Update of 'value
+    | NoAction
+
+module EditableValue =
+    let toFSharp<'value> (editable: Monads.DataOps.EditableValue<'value>): EditableValue<'value> =
+        editable.Match(
+            System.Func<'value, EditableValue<'value>>(Update),
+            System.Func<EditableValue<'value>>(fun _ -> NoAction))
+
+    let toCSharp<'value> (editable: EditableValue<'value>): Monads.DataOps.EditableValue<'value> =
+        match editable with
+        | Update value -> Monads.DataOps.EditableValue<'value>.Update(value)
+        | NoAction -> Monads.DataOps.EditableValue<'value>.NoAction()
+
+    let map (mapper: 'a -> 'b) (editable: EditableValue<'a>): EditableValue<'b> =
+        match editable with
+        | Update value -> Update (mapper value)
+        | NoAction -> NoAction
